69ed6f130e55b0452409e99a6e6721c8a70b0eb5
Daria Komzolova <d.komzolova@innopolis.university>
2026-03-17 22:58:05 +0300

    Первое соревнование в блоке 1. 2/2 задач решено!

--- src/blockOne/competitions/first/TaskA.java
+++ src/blockOne/competitions/first/TaskA.java
@@ -5,5 +5,38 @@ import java.util.Scanner;
 public class TaskA {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        scanner.nextLine();
+
+        int ans = 0;
+        char[][] arr = new char[n][m];
+
+        for (int i = 0; i < n; i++) {
+            String str = scanner.nextLine();
+            arr[i] = str.toCharArray();
+        }
+
+        for (int i = 0; i < n; i++) {
+            char[] temp = arr[i];
+
+            for (int j = 0; j < m - 1; j++) {
+                if (temp[j] == '.' && temp[j + 1] == '.') {
+                    ans++;
+                }
+            }
+
+            if (i < n - 1) {
+                char[] next = arr[i + 1];
+                for (int j = 0; j < m; j++) {
+                    if (temp[j] == '.' && next[j] == '.') {
+                        ans++;
+                    }
+                }
+            }
+        }
+
+        System.out.println(ans);
     }
 }
--- src/blockOne/competitions/first/TaskB.java
+++ src/blockOne/competitions/first/TaskB.java
@@ -5,5 +5,33 @@ import java.util.Scanner;
 public class TaskB {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+
+        int[] l = new int[n];
+        int[] r = new int[n];
+        int[] x = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            l[i] = scanner.nextInt();
+            r[i] = scanner.nextInt();
+            x[i] = scanner.nextInt();
+        }
+
+        for (int i = 0; i < m; i++) {
+            int q = scanner.nextInt();
+            long height = 0;
+
+            for (int j = 0; j < n; j++) {
+                if (q >= l[j] && q <= r[j]) {
+                    if ((q - l[j]) % 2 == 0) {
+                        height += x[j];
+                    } else {
+                        height -= x[j];
+                    }
+                }
+            }
+            System.out.println(height);
+        }
     }
 }
