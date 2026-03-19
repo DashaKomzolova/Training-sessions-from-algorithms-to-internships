0e147dd0ede58b6487e2e3ea339252928df63ab4
Daria Komzolova <d.komzolova@innopolis.university>
2026-03-19 22:55:38 +0300

    вторые соревнования тоже прошли успешно - 2/2

--- src/blockOne/competitions/second/TaskA.java
+++ src/blockOne/competitions/second/TaskA.java
@@ -6,5 +6,34 @@ public class TaskA {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int n = scanner.nextInt();
+        scanner.nextLine();
+        String str = scanner.nextLine();
+
+        int maxLen = 0;
+        int curLen = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c != 'a' && c != 'h') {
+                curLen = 0;
+            } else {
+                if (curLen == 0) {
+                    curLen = 1;
+                } else {
+                    char prev = str.charAt(i - 1);
+                    if (c != prev) {
+                        curLen++;
+                    } else {
+                        curLen = 1;
+                    }
+                }
+                if (curLen > maxLen) {
+                    maxLen = curLen;
+                }
+            }
+        }
+
+        System.out.println(maxLen);
     }
 }
--- src/blockOne/competitions/second/TaskB.java
+++ src/blockOne/competitions/second/TaskB.java
@@ -3,8 +3,36 @@ package blockOne.competitions.second;
 import java.util.Scanner;
 
 public class TaskB {
+
+    public static int func(long n, long m, long h, long w) {
+        int ans = 0;
+        while (n > h) {
+            n = (n + 1) / 2;
+            ans += 1;
+        }
+        while (m > w) {
+            m = (m + 1) / 2;
+            ans += 1;
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        long n = scanner.nextLong();
+        long m = scanner.nextLong();
+        long h = scanner.nextLong();
+        long w = scanner.nextLong();
+
+        if ((n <= h && m <= w) || (n <= w && m <= h)) {
+            System.out.println(0);
+        } else {
+
+            int ans1 = func(n, m, h, w);
+            int ans2 = func(m, n, h, w);
+
+            System.out.println(Math.min(ans1, ans2));
+        }
     }
 }
